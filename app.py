97b4bd79efd8a6fdb497b8156c3242c0f031be71
Samar Pratap Singh <35553535+Samar-080301@users.noreply.github.com>
2020-12-08 17:34:11 +0530

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-app = Flask(__name__)
-@app.route('/')
-def index():
-    #a = input('Enter name')
-    return render_template(r'form.html')
-
-if __name__ == "__main__":
-     app.run(debug=True)
